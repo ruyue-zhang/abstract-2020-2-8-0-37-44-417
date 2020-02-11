1d65bf9cecd45d402da42079963e2b4b15dbd0ba
lpdft <772346730@qq.com>
2020-02-11 11:57:40 +0800

    [ruyue] feat: create Chicken abstract class

--- abstract/src/main/java/com/Cock.java
+++ abstract/src/main/java/com/Cock.java
@@ -1,11 +1,9 @@
 package com;
 
 
-public class Cock {
+public class Cock extends Chicken {
 
-  public double getPrice() {
-    return 5;
-  }
+  public double getPrice() { return 5; }
 
   public String getType() {
     return getClass().getSimpleName();
