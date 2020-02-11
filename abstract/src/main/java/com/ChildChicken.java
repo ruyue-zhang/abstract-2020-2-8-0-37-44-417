e755fc10aa7e93ff2b4d987da32c7ae8c5df9335
lpdft <772346730@qq.com>
2020-02-11 17:59:04 +0800

    [ruyue] refactor: fixed ChildChicken class to abstract class

--- abstract/src/main/java/com/ChildChicken.java
+++ abstract/src/main/java/com/ChildChicken.java
@@ -1,12 +1,8 @@
 package com;
 
-public class ChildChicken {
+public class ChildChicken extends Chicken{
 
   public double getPrice() {
     return 1;
-  }
-
-  public String getType() {
-    return getClass().getSimpleName();
   }
 }
